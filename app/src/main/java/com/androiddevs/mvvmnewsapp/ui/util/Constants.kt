e78ce9faeeebe5251f9173e02c0842ceba302b96
aman kumar sinha <amankumarsinha1705j9@gmail.com>
2021-04-11 23:39:03 +0530

    first

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.ui.util
+
+class Constants {
+    companion object {
+        const val API_KEY="2cd34798ef944e5997e8e9819b43d734"
+        const val BASE_URL="https://newsapi.org"
+        const val  SEARCH_NEWS_TIME_DELAY= 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
